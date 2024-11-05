4080adcd3d1dfadf221f65ff94eeef96fef2a829
Batuhanirmali <batuqwert1@gmail.com>
2024-11-05 19:03:55 +0300

    Initial Commit

---
+++ ShopScreens/ShopScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopScreensApp.swift
+//  ShopScreens
+//
+//  Created by Talha Batuhan Irmalı on 5.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShopScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
